5c7db86f9efd1f9a3cc1b51fe68f28f7f3a51ce9
Muhammad Ahmad <muhammad.ahmad@qbatch.com>
2023-12-28 16:51:28 +0500

    :recycle: Pick API key from env

--- app.py
+++ app.py
@@ -1,42 +1,46 @@
 import time
+import os
 import shelve
 from flask import Flask, request
 from openai import OpenAI
 from constants import SPINNY_INSTRUCTION
 
 app = Flask(__name__)
-# FILE_ID = 'file-gdPgVsLFaAN2yDQyAeKWw7b5'
-# FILE_ID = 'file-DSZE2TK7wwggRjkH7Dn3o7li'  # File provided to assistant #old
-# ASSISTANT_ID = 'asst_MkjOHJCh90zr9slPv7RxQkdI' #old
 
-FILE_ID = 'file-1fLKeUnCjFyu7ATXcmxxnCmw'  # new
-ASSISTANT_ID = 'asst_gmrlgUwGJktMrG6lXtFIhfaK'  # new
+open_ai_key = os.environ.get('OPENAI_API_KEY')
+assistant_id = os.environ.get('ASSISTANT_ID')
+file_id = os.environ.get('FILE_ID')
 
-# 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
-
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-
-client = OpenAI(api_key='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')
-# Fahad bhai's account
-# client = OpenAI(api_key='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')
+client = OpenAI(api_key=open_ai_key)
 
 
 def upload_file(path):
-    # Upload a file with an "assistants" purpose
-    file = client.files.create(file=open(path, "rb"), purpose="assistants")
-    return file
+    """
+    Upload a file with an "assistants" purpose.
+
+    Args:
+        path (str): The path to the file to be uploaded.
+
+    Returns:
+        dict: The file information returned by OpenAI API.
+    """
+    return client.files.create(file=open(path, "rb"), purpose="assistants")
 
 
-def create_assistant(assistant_name,
-                     my_instruction,
-                     uploaded_file,
-                     model="gpt-4-1106-preview"):
+def create_assistant(assistant_name, my_instruction, uploaded_file, model="gpt-4-1106-preview"):
+    """
+    Create an OpenAI assistant.
 
-    my_assistant = client.beta.assistants.create(
+    Args:
+        assistant_name (str): The name of the assistant.
+        my_instruction (str): Instructions for the assistant.
+        uploaded_file (dict): The file information returned by OpenAI API.
+        model (str): The language model to use (default is "gpt-4-1106-preview").
+
+    Returns:
+        dict: The created assistant information returned by OpenAI API.
+    """
+    return client.beta.assistants.create(
         name=assistant_name,
         instructions=my_instruction,
         model=model,
@@ -44,92 +48,90 @@ def create_assistant(assistant_name,
         file_ids=[uploaded_file.id]
     )
 
-    return my_assistant
 
+def check_or_create_thread(wa_id, name, file_id):
+    """
+    Check if a thread exists for a given user ID; if not, create a new thread.
 
-def check_if_thread_exists(wa_id):
-    with shelve.open("threads_db") as threads_shelf:
-        return threads_shelf.get(wa_id, None)
+    Args:
+        wa_id (str): The user ID.
+        name (str): The user's name.
+        file_id (str): The file ID to be associated with the thread.
 
-
-def store_thread(wa_id, thread_id):
+    Returns:
+        str: The thread ID.
+    """
     with shelve.open("threads_db", writeback=True) as threads_shelf:
-        threads_shelf[wa_id] = thread_id
+        thread_id = threads_shelf.get(wa_id, None)
+        if thread_id is None:
+            thread = client.beta.threads.create(messages=[
+                {"role": "user", "content": f"Hey, I'm a new user to spinny. My name is {name}. Please greet me cheerfully.",
+                    "file_ids": [file_id]}
+            ])
+            threads_shelf[wa_id] = thread.id
+            return thread.id
+        else:
+            return thread_id
 
 
 @app.route('/chat',  methods=["POST"])
 def generate_response():
-    # Check if there is already a thread_id for the wa_id
+    """
+    Generate a response for a chat message from the user.
+
+    Returns:
+        str: The generated response from the assistant.
+    """
     data = request.get_json()
-    message_body = data['message']
+    message_body, wa_id, name = data['message'], data['user_id'], data['name']
 
-    # We will get these two from our DB.
-    wa_id = data['user_id']
-    name = data['name']
+    thread_id = check_or_create_thread(wa_id, name, file_id)
 
-    thread_id = check_if_thread_exists(wa_id)
-
-    # If a thread doesn't exist, create one and store it
-    if thread_id is None:
-        print(f"Creating new thread for {name} with wa_id {wa_id}")
-        thread = client.beta.threads.create(messages=[
-            {
-                "role": "user",
-                "content": f"Hey, I'm a new user to spinny. My name is {name}. Please greet me cheerfully.",
-                "file_ids": [FILE_ID]
-            }
-        ])
-        store_thread(wa_id, thread.id)
-        thread_id = thread.id
-
-    # Otherwise, retrieve the existing thread
-    else:
-        print(f"Retrieving existing thread for {name} with wa_id {wa_id}")
-        thread = client.beta.threads.retrieve(thread_id)
-
-    # Add message to thread
+    thread = client.beta.threads.retrieve(thread_id)
     message = client.beta.threads.messages.create(
         thread_id=thread_id,
         role="user",
         content=message_body,
-        file_ids=[FILE_ID]  # we can override the assistant file here too
+        file_ids=[file_id]
     )
 
-    # Run the assistant and get the new message
     new_message = run_assistant(thread)
     print(f"To {name}, Thread {thread_id}:", new_message)
     return new_message
 
 
 def run_assistant(thread):
-    # Retrieve the Assistant
+    """
+    Run the OpenAI assistant for a given thread.
 
+    Args:
+        thread (dict): The thread information.
+
+    Returns:
+        str: The generated message from the assistant.
+    """
     run = client.beta.threads.runs.create(
         thread_id=thread.id,
-        assistant_id=ASSISTANT_ID,
+        assistant_id=assistant_id,
         instructions=SPINNY_INSTRUCTION
     )
 
-    # Wait for completion
     while run.status != "completed":
         time.sleep(0.5)
         run = client.beta.threads.runs.retrieve(
             thread_id=thread.id, run_id=run.id)
 
-    # Retrieve the Messages
     messages = client.beta.threads.messages.list(thread_id=thread.id)
     new_message = messages.data[0].content[0].text.value
     print(f"Generated message: {new_message}")
     return new_message
 
 
+# Use the code below if a new assistant is created or API key is changed
 inst = SPINNY_INSTRUCTION
 assistant_name = "Spinny the friendly AI bot"
 uploaded_file = upload_file('teach_spinny.pdf')
-# FILE_ID = uploaded_file.id
-
 my_assistant = create_assistant(assistant_name, inst, uploaded_file)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
